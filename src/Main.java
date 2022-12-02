2d354e345ae21d65232fc8d13540ec49a9043e87
Lidier López <llopezraccioppe@danielcastelao.org>
2022-12-02 10:04:15 +0100

    soy el main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ public class Main {
         public static void main(String[] args) {
                 System.out.println("Inicio del proyecto");
 
+                // Otro experimento
 
-
-
-
+                // Soy el Main
         }
 }
